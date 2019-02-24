8b684eb860d6cba19a65eb63f03e65f748065191
hugoli <hugoli@kth.se>
2019-02-24 23:55:41 +0100

    Add files via upload
    Added short code for setting the Baud rate of your bluetooth module

---
+++ setBTBaud.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); //this Baud rate (byte frequency) needs to be set as to match the set Baud rate that the Bluetooth module currently, and just previously, operates on. 
+}
+
+void loop() {
+  //Serial.print("AT+BAUD4"); //communicate with the bluetooth module, set to 9600, for more information see for example https://www.instructables.com/id/AT-command-mode-of-HC-05-Bluetooth-module/
+
+}
